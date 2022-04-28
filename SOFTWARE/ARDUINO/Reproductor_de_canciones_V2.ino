ec6f5d50ed82981e9eac23ad90a742f6f5be2ecd
JavierLerinAlonso <80424307+JavierLerinAlonso@users.noreply.github.com>
2022-04-28 18:27:02 +0200

    Update Reproductor_de_canciones_V2.ino

--- SOFTWARE/ARDUINO/Reproductor_de_canciones_V2.ino
+++ SOFTWARE/ARDUINO/Reproductor_de_canciones_V2.ino
@@ -168,6 +168,62 @@ int duracion[]={
   500,500,1000 
 };
  */   
+/*Cancion de Super Mario
+const float velocidadcancion = 1.25;
+int notas[] ={ 
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+  
+};
+int duracion[]={
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+ 
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+ 
+  150, 150, 150,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+ 
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+ 
+  150, 150, 150,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+  120, 120, 120, 120,
+};
+*/
 void setup()
 {
   const int totalNotas = sizeof(notas) / sizeof(int);
